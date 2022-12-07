8936e0ff18cc75df0a64926b3272706a1101a182
byseop <byseop@gmail.com>
2022-12-07 17:18:33 +0900

    feat: 카드리스트 추가

--- src/components/Smilepay/CardList/CardList.Styled.ts
+++ src/components/Smilepay/CardList/CardList.Styled.ts
@@ -1,6 +1,62 @@
 import styled from 'styled-components';
 import CardList from './CardList';
 
-const StyledCardList = styled(CardList)``;
+const StyledCardList = styled(CardList)`
+  padding: 15px;
+  ul {
+    display: flex;
+    flex-flow: column;
+    gap: 10px;
+
+    li {
+      position: relative;
+      background: var(--white);
+      display: flex;
+      border: 1px solid var(--border-gary-1);
+      border-radius: 4px;
+      padding: 15px;
+      gap: 15px;
+      cursor: pointer;
+      align-items: center;
+
+      .card-image {
+        font-size: 4rem;
+        color: var(--bg-securekeypad-1);
+        svg {
+          vertical-align: middle;
+        }
+      }
+
+      .card-info {
+        font-size: 1.3rem;
+
+        .card-number {
+          margin-top: 5px;
+        }
+      }
+
+      .btn-remove {
+        position: absolute;
+        right: 15px;
+        top: 50%;
+        transform: translateY(-50%);
+
+        button {
+          background: none;
+          border: none;
+          padding: 0;
+          margin: 0;
+          cursor: pointer;
+        }
+      }
+
+      .no-card-message {
+        font-size: 1.5rem;
+        width: 100%;
+        text-align: center;
+      }
+    }
+  }
+`;
 
 export default StyledCardList;
--- src/components/Smilepay/CardList/CardList.tsx
+++ src/components/Smilepay/CardList/CardList.tsx
@@ -1,11 +1,56 @@
+import { useLocalStorage } from 'usehooks-ts';
+import { FaCcVisa, FaTrash } from 'react-icons/fa';
+
+import type { IFormState } from '../AddCardForm';
+
 interface IProps {
   className?: string;
 }
 
 const CardList: React.FC<IProps> = ({ className }) => {
+  const [cardList, setCardList] = useLocalStorage<IFormState[]>(
+    'smilepayCardList',
+    []
+  );
+
+  /** 카드 삭제버튼  클릭이벤트 */
+  const handleClickRemoveCard = (id: string) => {
+    setCardList((prev) => prev.filter((card) => card.id !== id));
+  };
+
   return (
     <div className={`${className} card-list`}>
-      <ul></ul>
+      <ul>
+        {cardList.length === 0 && (
+          <li>
+            <p className="no-card-message">카드를 등록해주세요.</p>
+          </li>
+        )}
+        {cardList.map((card) => (
+          <li key={card.id}>
+            <div className="card-image">
+              <FaCcVisa />
+            </div>
+            <div className="card-info">
+              <div className="card-name">
+                <p>아무은행 카드</p>
+              </div>
+              <div className="card-number">
+                <p>
+                  {card.cardNum1} {card.cardNum2.slice(0, 2)}** ****{' '}
+                  {card.cardNum4.slice(0, 3)}*
+                </p>
+              </div>
+            </div>
+
+            <div className="btn-remove">
+              <button onClick={() => handleClickRemoveCard(card.id)}>
+                <FaTrash />
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
